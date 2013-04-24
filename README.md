8d292419cd250cddb5fd0b8f133f06a5b4ebbb27
da99 <i-hate-spam-1234567@mailinator.com>
2013-04-24 08:34:20 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,44 @@
+
+Intro:
+=====
+
+This is not useful to you. Instead, use the following for data binding and
+model watching:
+
+  * [Ember.js](http://emberjs.com)
+  * [Rivets.js](http://rivetsjs.com)
+  * [Knockout.js](http://knockoutjs.com)
+  * [Thorax.js](http://thoraxjs.org)
+  * ... and a million other alternatives.
+
+Dependencies
+============
+
+  * jQuery >= 1.9.1+
+  * Underscore >= 1.4.3
+  * Backbone >= 1.0.0
+
+Install:
+========
+
+Latest version:
+
+    wget -O "Hyper_JS.js" https://raw.github.com/da99/Hyper_JS/master/Hyper_JS.js
+
+In your HTML:
+
+    <script src="../path/to/jQuery"></script>
+    <script src="./Hyper_JS.js" type="text/javascript"></script>
+
+
+Running Tests:
+=============
+
+     git clone git://github.com/da99/Hyper_JS.git
+     cd Hyper_JS
+     # open file in your browser: Tests.html
+
+
+
+
+
---
+++ middle.js
@@ -0,0 +1,53 @@
+
+var results = {pass: 0, fail: 0, error: 0};
+var test_name = null;
+
+function test (name, func) {
+  test_name = name;
+  try {
+    func();
+  } catch (e) {
+    error(name, e);
+  }
+}
+
+function span(txt) {
+  return '<span>' + (txt + '').replace(/</g, '&lt;').replace(/>/g, '&gt;') + '</span>';
+}
+function passed(name, exp, act) {
+  results.pass = results.pass + 1;
+  $('#results').append('<div class="passed">' + span(exp) + ' === ' + span(act) + '<br /> ' + name + '</div>');
+}
+
+function failed(name, exp, act) {
+  results.fail = results.fail + 1;
+  $('#results').append('<div class="failed">' + span(exp) + ' === ' + span(act) + '<br /> ' + name + '</div>');
+}
+
+function error(name, act) {
+  results.error = results.error + 1;
+  $('#results').append('<div class="failed">' + span(act) + '<br /> ' + name + '</div>');
+}
+
+function assert (exp, act, name) {
+  if (exp === act)
+    passed(name || test_name, exp, act);
+  else
+    failed(name || test_name, exp, act)
+
+}
+
+assert.deepEqual = function (a, b, name) {
+  name = name || test_name;
+  var j_a = JSON.stringify(a);
+  var j_b = JSON.stringify(b);
+  try {
+    proclaim.deepEqual(a, b);
+    passed(name, j_a, j_b);
+  } catch (e) {
+    error(name, j_a + ' !== ' + j_b);
+  }
+};
+
+
+
