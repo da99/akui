2c3c537aab26af271cb002edc8b1d65c754c9e9c
da99 <i-hate-spam-1234567@mailinator.com>
2013-04-27 04:51:36 -0500

    More implementation.

--- public/akui.js
+++ public/akui.js
@@ -10,11 +10,13 @@ Akui.report = {
   pass  : [],
   fail  : [],
   error : [],
+  waits : [],
   all   : []
 };
 
 Akui.current = {
-  test_name : null
+  test_name : null,
+  test_index : -1
 };
 
 // ================================================================
@@ -22,51 +24,32 @@ Akui.current = {
 // ================================================================
 
 
-Akui.pass(msg)  { Akui.result('pass', msg); };
-Akui.fail(msg)  { Akui.result('fail', msg); };
-Akui.error(msg) { Akui.result('error', msg); };
+Akui.pass(name, msg)  { Akui.result('pass',  name, msg); };
+Akui.fail(name, msg)  { Akui.result('fail',  name, msg); };
+Akui.error(name, msg) { Akui.result('error', name, msg); };
 
-Akui.result = function (type, _args_) {
-  Akui.report.all.push(arguments);
-  Akui.report[(type === 'pass' || type === 'fail') ? type : 'error'].push(arguments);
-  if (_args_.message && _args_.constructor && _args_.constructor === Error)
-    throw _args_;
+Akui.result = function (type, name, exp, act) {
+  var args = Array.prototype.slice.apply(arguments, []);
+
+  if (Akui.report.waits[name]) {
+    var test = Akui.report.waits[name];
+    name         = args[1] = test.name;
+    test.fin     = true;
+    test.results = args;
+  }
+
+  Akui.report.all.push(args);
+  Akui.report[(type === 'pass' || type === 'fail') ? type : 'error'].push(args);
+
+  var is_err = exp.message && exp.constructor && exp.constructor === Error;
+  if (is_err)
+    throw exp;
+
   return Akui;
 };
 
 window.onerror = Akui.error;
 
-
-// ================================================================
-// ================== Testing =====================================
-// ================================================================
-
-
-function assert (exp, act, name) {
-  var a = JSON.stringify(exp);
-  var b = JSON.stringify(act);
-  if (_.isEqual(exp, act))
-    Akui.pass((name || Akui.current.test_name), a, b);
-  else
-    Akui.fail((name || Akui.current.test_name), a, b);
-}
-
-Akui.test = function (name, func) {
-
-  var hash = (window.location.hash !== '') ? window.location.hash.replace('#', '') : null;
-
-  if (hash && name.indexOf(hash) < 0)
-    return;
-
-  Akui.current.test_name = name;
-
-  try {
-    func();
-  } catch (e) {
-    Akui.error(e);
-  }
-
-};
 
 // ================================================================
 // ================== Printing ====================================
@@ -81,8 +64,8 @@ Akui.print.all = function() {
   else if (Akui.report.pass.length > 0)
     console.log("ALL PASS.");
 
-  _.each(Akui.report.all, function (args) {
-    print_report.apply(null, args);
+  _.each(Akui.report.all, function (test) {
+    Akui.print.report.apply(null, test.result);
   });
 }
 
@@ -110,5 +93,70 @@ Akui.print.report = function(type, name, exp, act) {
     break;
 
   };
-}
+};
+
+
+// ================================================================
+// ================== Testing =====================================
+// ================================================================
+
+Akui.test = function (name, func) {
+
+  var hash = (window.location.hash !== '') ? window.location.hash.replace('#', '') : null;
+
+  if (hash && name.indexOf(hash) < 0)
+    return;
+
+  Akui.current.test_index += 1;
+  var index = Akui.current.test_index;
+
+  var assert = function (exp, act) {
+    var a = JSON.stringify(exp);
+    var b = JSON.stringify(act);
+    if (_.isEqual(exp, act))
+      Akui.pass(index, a, b);
+    else
+      Akui.fail(index, a, b);
+  };
+
+
+  try {
+    Akui.report.waits[Akui.current.test_index] = {name: name, results: null, fin: null};
+    func(assert);
+  } catch (e) {
+    Akui.error(index, e);
+  }
+
+};
+
+Akui.run_next = function () {
+  microAjax('/akui_tests/next', function (str) {
+    alert(typeof str);
+  });
+};
+
+// ================================================================
+// ================== Showtime! ===================================
+// ================================================================
+
+Akui.run_next();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
