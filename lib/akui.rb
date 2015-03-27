b0b46c5aeab652fe718210330d2c4cfb0da66928
da99 <i-hate-spam-1234567@mailinator.com>
2015-03-27 13:27:03 -0500

    Obsolete code: Let the user decide when to include JS src file.

--- lib/akui.rb
+++ lib/akui.rb
@@ -4,25 +4,6 @@ require 'escape_escape_escape'
 
 class Akui
 
-  # def initialize app
-    # @app
-  # end
-
-  # def call env
-    # dup._call env
-  # end
-
-  # def _call env
-    # status, headers, body = @app.call env
-    # is_html  = headers['Content-Type'] && headers['Content-Type']['html']
-    # end_body = is_html && body.detect { |b| b['</body>'] }
-    # return [status, headers, body] unless is_html && end_body
-    # end_body.sub!('</body>', <<-EOF.strip)
-      # <script src="/akui/akui.js"></script></body>
-    # EOF
-    # [status, headers, body]
-  # end
-
   class Middleware < Cuba
 
     define {
